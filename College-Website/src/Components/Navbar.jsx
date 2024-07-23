b34bdeeaa0a5fded780cbc7a0aaa281614d72406
Dilipmandadi27 <148129250+Dilipmandadi27@users.noreply.github.com>
2024-07-24 03:47:33 +0530

    responsive ui done pending with scroll & render and mobile menubar

--- College-Website/src/Components/Navbar.jsx
+++ College-Website/src/Components/Navbar.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
-import logo from "../assets/logo.png"
-import { useState,useEffect } from 'react'
-
+import React from "react";
+import logo from "../assets/logo.png";
+import { useState, useEffect } from "react";
+import menu_icon from "../assets/menu-icon.png";
 
 const Navbar = () => {
+  const [Sticky, setSticky] = useState(false);
 
-const [Sticky, setSticky] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
 
-useEffect(()=>{
- window.addEventListener('scroll', ()=>{
-   window.scrollY > 50 ? setSticky(true) : setSticky(false)
- })
-},[])
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
 
   return (
     <nav className={`container ${Sticky ? "dark-nav" : ""} `}>
-        
       <div className="logo">
-          <img src={logo} alt="brand-logo" />
+        <img src={logo} alt="brand-logo" />
       </div>
       <div className="navigations">
-        <a href="#Home">Home</a>
-        <a href="#Programs">Programs</a>
-        <a href="#About Us">About Us</a>
-        <a href="#Campus">Campus</a>
-        <a href="#Testimonials">Testimonials</a>
-        <button className='btn'>Contact Us</button>
-        </div>
-    
+        <ul className={mobileMenu ? "" : "hideMenubar"}>
+          <li>Home</li>
+          <li>Programs</li>
+          <li>About Us</li>
+          <li>Campus</li>
+          <li>Testimonials</li>
+          <button className="btn">Contact Us</button>
+        </ul>
+        <img
+          src={menu_icon}
+          className="menubar_icon"
+          alt=""
+          onClick={toggleMenu}
+        />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
